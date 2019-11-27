9924fbb11905d66a56b2a99d21ddb531a0649eb9
Tom Walker <tomjwalker@github.com>
2019-11-27 17:49:42 +0000

    Fixed bug where dependent variable added to features

--- disc/dataset_visualiser.py
+++ disc/dataset_visualiser.py
@@ -12,19 +12,22 @@ def correlation_plotter(corr_plot):
     mask[np.triu_indices_from(mask)] = True
 
     # Generate a custom diverging colormap
-    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    cmap = sns.color_palette("RdBu_r", 9)
 
     ax = sns.heatmap(corr_plot, mask=mask, cmap=cmap, vmax=1, vmin=-1, center=0,
                 square=True, linewidths=.5, annot=True, fmt='.2f')
 
     
 def correlation_analyser(df_encoded, label, n_top_correlating_features=10, plot=True):
+    
+    # Create a copy of the input dataframe
+    df_corr = df_encoded.copy()
 
     # Create a full dataset of encoded features alongside the dependent variable (label)
-    df_encoded['dependent_variable'] = label
+    df_corr['dependent_variable'] = label
 
     # Get correlation matrix
-    corr = df_encoded.corr()
+    corr = df_corr.corr()
 
     # Get absolute correlation strength for sorting (care equally about positive and negative correlations to the label)
     corr['abs_dependent_variable'] = corr['dependent_variable'].apply(lambda x: np.abs(x))
