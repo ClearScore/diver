19e9c9e8694691d46e2a3d7d3ec066fd91986783
Tom Walker <tomjwalker@github.com>
2020-01-16 15:37:18 +0000

    made a start towards unit tests

--- development_area/dataset_inspector.py
+++ development_area/dataset_inspector.py
@@ -7,16 +7,16 @@ from diver._shared import _get_boolean_elements
 # USEFUL_COLS AUTO-GENERATOR #
 ##############################
 
-def _get_boolean_elements():
-    '''
-    Returns sets of elements recognisable as booleans
-    '''
+# def _get_boolean_elements():
+#     '''
+#     Returns sets of elements recognisable as booleans
+#     '''
     
-    # Inputs to be recognised as booleans
-    true_set = {'True','true','TRUE','tru',1,'t','T','1', float(1), True, 'yes', 'Yes', 'YES', 'Y', 'y'}
-    false_set = {'False','false','FALSE','fals',0,'f','F','0', float(0), False, 'no', 'No', 'NO', 'N', 'n'}
+#     # Inputs to be recognised as booleans
+#     true_set = {'True','true','TRUE','tru',1,'t','T','1', float(1), True, 'yes', 'Yes', 'YES', 'Y', 'y'}
+#     false_set = {'False','false','FALSE','fals',0,'f','F','0', float(0), False, 'no', 'No', 'NO', 'N', 'n'}
     
-    return true_set, false_set
+#     return true_set, false_set
 
 
 def _dtype_detector(features):
