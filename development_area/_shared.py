a4bb009ed30c6a292bdfe2f3d93c1f237685e36e
Tom Walker <tomjwalker@github.com>
2020-01-23 16:50:11 +0000

    bug fix so output df indices match input df indices

--- development_area/_shared.py
+++ development_area/_shared.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 def _get_boolean_elements():
     '''
     Returns sets of elements recognisable as booleans
@@ -7,6 +5,6 @@ def _get_boolean_elements():
     
     # Inputs to be recognised as booleans
     true_set = {'True','true','TRUE','tru',1,'t','T','1', float(1), True, 'yes', 'Yes', 'YES', 'Y', 'y'}
-    false_set = {'False','false','FALSE','fals',0,'f','F','0', float(0), False, 'no', 'No', 'NO', 'N', 'n', np.nan}
+    false_set = {'False','false','FALSE','fals',0,'f','F','0', float(0), False, 'no', 'No', 'NO', 'N', 'n'}
     
     return true_set, false_set
